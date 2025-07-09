0f6b26897c4a860b7e194845216d4bb55a57284f
Felipe Gabriel Menezes Lacerda <felipegabrielmenezeslacerda@gmail.com>
2025-07-09 10:13:58 -0300

    Implementando algumas interações para o bichinho e mais instruções de interação

--- VirtualPet/VirtualPet/Program.cs
+++ VirtualPet/VirtualPet/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace VirtualPet
 {
@@ -50,8 +47,17 @@ namespace VirtualPet
         (string petName, string petOwner, ref int petHunger, ref int petHappiness, ref int petCleanliness)
         {
             string userEntry = "";
+            string[] petConversation = new string[3];
+            petConversation[0] = "Hoje foi um dia muitooo legal, comi o sofá";
+            petConversation[1] = "Estava com saudade de você, passei o dia te esperando";
+            petConversation[2] = "HyaBadabaDuh";
+
             int petStatus = 0;
             Random random = new Random();
+
+            Console.WriteLine("A tecla 'Enter' é a tecla utilizada para continuar no jogo, ou para enviar as respostas");
+            Thread.Sleep(2000);
+            Console.Clear();
 
             Console.WriteLine($"Olá {petOwner}, senti sua falta!!!");
             while (userEntry != "4")
@@ -69,14 +75,14 @@ namespace VirtualPet
 
                 Console.Clear();
                 Console.WriteLine
-                    ($"Fome: {petHunger} | Alegria: {petHappiness} | Limpo: {petCleanliness}");
-
+                    ($"Fome: {petHunger} | Alegria: {petHappiness} | Limpo: {petCleanliness}\n");
+                
                 Console.WriteLine("O que vamos fazer hoje?");
                 Console.WriteLine("1.Brincar");
                 Console.WriteLine("2.Comer");
                 Console.WriteLine("3.Tomar banho");
                 Console.WriteLine("4.Nada");
-                Console.Write("Digite o número da opção desejada: ");
+                Console.Write("Digite o número da opção desejada e clique a tecla 'Enter' para prosseguir: ");
                 userEntry = Console.ReadLine();
 
                 switch (userEntry)
@@ -89,9 +95,25 @@ namespace VirtualPet
                         break;
                 }
 
+                Console.Clear();
+
                 if(petHunger > 100) { petHunger = 100; }
                 if(petHappiness > 100) { petHappiness = 100; }
                 if(petCleanliness > 100) { petCleanliness = 100; }
+
+                if (petHunger < 60 && petHunger > 20) Console.WriteLine("Que fome, nada melhor que um lanchinho");
+                if (petHappiness < 60 && petHappiness > 20) Console.WriteLine("Estou tão desanimado, nada melhor do que brincar");
+                if (petCleanliness < 60 && petCleanliness > 20) Console.WriteLine("Estou tão sujo, nada melhor que um banho");
+                
+                if (!(petHunger < 60 && petHunger > 20) && 
+                    !(petHappiness < 60 && petHappiness > 20) && 
+                    !(petCleanliness < 60 && petCleanliness > 20))
+                {
+                    Console.WriteLine(petConversation[random.Next(petConversation.Length)]);
+                }
+
+                Thread.Sleep(2000);
+
             }
 
             
